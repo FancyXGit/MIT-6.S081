00ea6ab989619046493061bf95abd1022066f4f5
FancyXGit <fancy.fantasy688@gmail.com>
2026-08-31 20:57:29 +0800

    primes V2

--- user/primes.c
+++ user/primes.c
@@ -2,56 +2,75 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Winfinite-recursion"
+
+void prime(void)
+{
+    int p[2];
+    pipe(p);
+
+    int prime_num;
+    if (read(0, &prime_num, sizeof(int)) == 0)
+    {
+        exit(0);
+    }
+
+    printf("prime %d\n", prime_num);
+
+    if (fork() == 0)
+    {
+        close(0);
+        dup(p[0]);
+        close(p[0]);
+        close(p[1]);
+        prime();
+        exit(0);
+    }
+    else
+    {
+        close(p[0]);
+        int num;
+        while (read(0, &num, sizeof(int)))
+        {
+            if (num % prime_num != 0)
+            {
+                write(p[1], &num, sizeof(int));
+            }
+        }
+        close(p[1]);
+        wait(0);
+        exit(0);
+    }
+}
+
+#pragma GCC diagnostic pop
+
 int main(int argc, char *argv[])
 {
-    // 2、3、5、7、11、13、17、19、23、29、31
+    int p[2];
+    pipe(p);
 
-    int number[48];
-    for (int i = 0; i < 11; i++)
+    if (fork() == 0)
     {
-        // pipes为读端指向第i个进程的管道
-        int pipes[2];
-        pipe(pipes);
-
-        if (fork() == 0)
-        {
-            close(pipes[1]);
-            read(pipes[0], number, sizeof(number));
-            close(pipes[0]);
-
-            int size_read = number[0];
-            int ptr_write = 1;
-            number[0] = 0;
-            int prime = number[1];
-            printf("prime %d\n", prime);
-            for (int ptr_read = 1; ptr_read <= size_read; ptr_read++)
-            {
-                if (number[ptr_read] % prime != 0)
-                {
-                    number[0]++;
-                    number[ptr_write] = number[ptr_read];
-                    ptr_write++;
-                }
-            }
-
-            continue;
-        }
-        else
-        {
-            if (i == 0)
-            {
-                number[0] = 34;
-                for (int j = 1; j < 35; j++)
-                {
-                    number[j] = j + 1;
-                }
-            }
-            close(pipes[0]);
-            write(pipes[1], number, (number[0] + 1) * sizeof(int));
-            close(pipes[1]);
-            wait(0);
-            break;
-        }
+        close(0);
+        dup(p[0]);
+        close(p[0]);
+        close(p[1]);
+        prime();
+        exit(0);
     }
-    exit(0);
+    else
+    {
+        close(p[0]);
+
+        for (int i = 2; i < 36; i++)
+        {
+            write(p[1], &i, sizeof(int));
+        }
+
+        close(p[1]);
+        wait(0);
+        exit(0);
+    }
 }
