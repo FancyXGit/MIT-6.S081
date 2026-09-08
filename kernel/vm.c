b70f1b9413b21bb958c3a42b3be3841162218c9d
FancyXGit <fancy.fantasy688@gmail.com>
2026-09-08 16:34:46 +0800

    vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,37 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint_println(int depth, int index, pte_t pte)
+{
+  uint64 pa = PTE2PA(pte);
+  const char *dots[3] = {"..", ".. ..", ".. .. .."};
+  printf(" %s%d: pte %p pa %p\n", dots[depth], index, pte, pa);
+}
+
+void vmprint_helper(pagetable_t pagetable, int depth)
+{
+  for (int i = 0; i < 512; i++)
+  {
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V) && (pte & (PTE_R | PTE_W | PTE_X)) == 0)
+    {
+      // 指向下一个页表
+      vmprint_println(depth, i, pte);
+      vmprint_helper((pagetable_t)PTE2PA(pte), depth + 1);
+    }
+    else if (pte & PTE_V)
+    {
+      // 叶子节点
+      vmprint_println(depth, i, pte);
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", (uint64)pagetable);
+  vmprint_helper(pagetable, 0);
+}
+
+
